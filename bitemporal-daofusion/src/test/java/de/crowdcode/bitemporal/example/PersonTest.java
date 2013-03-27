d42db9895c91f86d3310960f88b8f1090ed1e6a0
lofi <lofidewanto@gmail.com>
2013-03-27 16:37:24 +0100

    update test

--- bitemporal-daofusion/src/test/java/de/crowdcode/bitemporal/example/PersonTest.java
+++ bitemporal-daofusion/src/test/java/de/crowdcode/bitemporal/example/PersonTest.java
@@ -21,6 +21,12 @@ package de.crowdcode.bitemporal.example;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.anasoft.os.daofusion.bitemporal.TimeUtils;
 
 /**
  * Unit test for simple app.
@@ -29,13 +35,27 @@ import org.junit.Test;
  * @since 1.0.0
  * @version 1.0.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:de/crowdcode/bitemporal/example/spring-example.xml" })
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 public class PersonTest {
 
-    /**
-     * Rigourous Test.
-     */
-    @Test
-    public void testApp() {
-      assertTrue(true);
-    }
+	@Test
+	public void testCreateBitemporalAdressen() {
+		assertTrue(true);
+
+		PersonImpl person = new PersonImpl();
+		TimeUtils.setReference(TimeUtils.day(1, 1, 2010));
+
+		Adresse firstAdresse = new AdresseImpl();
+		Adresse secondAdresse = new AdresseImpl();
+
+		// First Order will be valid from now on (1-Jan-2010 .. end_of_time)
+		person.getBitemporalAdressen().set(firstAdresse);
+
+		// Second Order supersedes the first one:
+		// - First order valid in [1-Jan-2010 .. 10-Feb-2010]
+		// - Second order valid in [10-Feb-2010 .. end_of_time]
+		person.getBitemporalAdressen().set(secondAdresse, TimeUtils.from(TimeUtils.day(10, 2, 2010)));
+	}
 }
