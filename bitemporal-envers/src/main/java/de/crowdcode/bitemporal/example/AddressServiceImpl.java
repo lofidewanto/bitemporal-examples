628e82592c837f917d49ac472669fd8d0e04df6e
lofidewanto <lofidewanto@gmail.com>
2013-08-14 23:30:35 +0200

    add envers example

---
+++ bitemporal-envers/src/main/java/de/crowdcode/bitemporal/example/AddressServiceImpl.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.crowdcode.bitemporal.example;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Named("addressService")
+public class AddressServiceImpl implements AddressService {
+
+	@Inject
+	@Named("addressRepository")
+	private AddressRepository addressRepository;
+
+	@Inject
+	@Named("personService")
+	private PersonService personService;
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Address createAddress(Address address) {
+		Address addressCreated = addressRepository.save(address);
+		return addressCreated;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public Integer getAmountOfAddress() {
+		Integer amount = addressRepository.getAmount();
+		return amount;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public Collection<Address> findAllAddresses() {
+		Collection<Address> addresses = addressRepository.findAll();
+		return addresses;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Address createAddressWithPerson(Address address, Person person) {
+		Address addressCreated = addressRepository.save(address);
+		Person personFound = personService.findPersonById(person.getId());
+		personFound.setAddress(addressCreated);
+		return addressCreated;
+	}
+}
---
+++ bitemporal-envers/src/main/java/de/crowdcode/bitemporal/example/PersonImpl.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.crowdcode.bitemporal.example;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Person implementation.
+ * 
+ * @author Lofi Dewanto
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+@Entity
+@Table(name = "person")
+public class PersonImpl implements Person, Serializable {
+
+	private static final long serialVersionUID = 2909659022959145389L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "firstname")
+	private String firstname;
+
+	@Column(name = "lastname")
+	private String lastname;
+
+	@OneToOne(targetEntity = AddressImpl.class)
+	@JoinColumn
+	private Address address;
+
+	@Override
+	public String getFirstname() {
+		return firstname;
+	}
+
+	@Override
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	@Override
+	public Address getAddress() {
+		return this.address;
+	}
+
+	@Override
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String getLastname() {
+		return lastname;
+	}
+
+	@Override
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public Address address() {
+		return getAddress();
+	}
+
+	@Override
+	public Address alive() {
+		return getAddress();
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
