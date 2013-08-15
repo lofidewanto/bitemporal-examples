a9e7ca78e11b2eb04429eab5954bc0f28899dd51
lofi <lofidewanto@gmail.com>
2013-08-15 09:32:30 +0200

    move from bitemporal-envers to the correct place record-temporal-envers

---
+++ record-temporal-envers/src/main/java/de/crowdcode/bitemporal/example/AddressRepository.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.crowdcode.bitemporal.example;
+
+import java.util.Collection;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.hibernate.envers.AuditReader;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Named("addressRepository")
+public class AddressRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	private AuditReader ar;
+
+	public Address save(Address address) {
+		em.persist(address);
+		return address;
+	}
+
+	public Integer getAmount() {
+		Query query = em.createQuery("select count(c.id) from AddressImpl c");
+		Number amount = (Number) query.getSingleResult();
+		return amount.intValue();
+	}
+
+	@SuppressWarnings("unchecked")
+	public Collection<Address> findAll() {
+		Query query = em.createQuery("select c from AddressImpl c");
+		return query.getResultList();
+	}
+
+	public Address findById(Long id) {
+		Query query = em.createQuery("select c from AddressImpl c where c.id = :id");
+		query.setParameter("id", id);
+		return (Address) query.getSingleResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	public Collection<Address> findAuditedAdressesWithRevision(Long revisionNumber) {
+		Collection<Address> auditedAddresses = ar.createQuery().forEntitiesAtRevision(Address.class, 1).getResultList();
+		return auditedAddresses;
+	}
+}
