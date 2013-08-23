a6136419116e20cbec855f2bd3ae9414d1b87023
lofi <lofidewanto@gmail.com>
2013-08-23 11:46:36 +0200

    add test

--- actual-temporal/src/test/java/de/crowdcode/bitemporal/example/PersonImplTest.java
+++ actual-temporal/src/test/java/de/crowdcode/bitemporal/example/PersonImplTest.java
@@ -1,11 +1,13 @@
 package de.crowdcode.bitemporal.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -43,5 +45,64 @@ public class PersonImplTest {
 
 		// Asserts
 		assertNull(address);
+	}
+
+	@Test
+	public void testGetSingleAddressOneAddressesAvailable() {
+		// Prepare
+		Date currentDate = new Date();
+		AddressImpl firstAddress = new AddressImpl();
+		firstAddress.setStreet("Hauptstr. 21");
+		firstAddress.setCity("Koeln");
+		firstAddress.setCode("50698");
+		firstAddress.setValidFrom(currentDate);
+		Collection<AddressImpl> addresses = new ArrayList<AddressImpl>();
+		addresses.add(firstAddress);
+
+		when(
+				addressRepository.findByPersonIdAndValidity(anyLong(),
+						eq(currentDate))).thenReturn(addresses);
+
+		// CUT
+		Address address = personImpl.getSingleAddress(currentDate);
+
+		// Asserts
+		assertEquals("Koeln", address.getCity());
+	}
+
+	@Test
+	public void testGetSingleAddressMoreThanOneAddressesAvailable() {
+		// Prepare
+		Date currentDate = new Date();
+		AddressImpl firstAddress = new AddressImpl();
+		firstAddress.setStreet("Hauptstr. 21");
+		firstAddress.setCity("Koeln");
+		firstAddress.setCode("50698");
+		// Valid from 9.9.1999
+		Calendar cal = Calendar.getInstance();
+		cal.set(1999, Calendar.SEPTEMBER, 9);
+		firstAddress.setValidFrom(cal.getTime());
+
+		AddressImpl secondAddress = new AddressImpl();
+		secondAddress.setStreet("Grossmarkt 22");
+		secondAddress.setCity("Berlin");
+		secondAddress.setCode("10313");
+		// Valid from 8.8.2008
+		cal.set(2008, Calendar.AUGUST, 8);
+		secondAddress.setValidFrom(cal.getTime());
+
+		Collection<AddressImpl> addresses = new ArrayList<AddressImpl>();
+		addresses.add(firstAddress);
+		addresses.add(secondAddress);
+
+		when(
+				addressRepository.findByPersonIdAndValidity(anyLong(),
+						eq(currentDate))).thenReturn(addresses);
+
+		// CUT
+		Address address = personImpl.getSingleAddress(currentDate);
+
+		// Asserts
+		assertEquals("Berlin", address.getCity());
 	}
 }
